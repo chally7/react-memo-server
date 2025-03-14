e42e0c2e24fd95d2d12f1106c0792ccc936240ca
chally7 <itlab385@gmail.com>
2025-03-14 17:33:27 +0900

    first commi11t

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser')
 const test = require('./api/test');
+const memo = require('./api/memo');
+
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/test',test);
+app.use('/memo',memo);
 
 app.listen(3000)
